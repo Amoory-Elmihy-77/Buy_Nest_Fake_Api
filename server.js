913fa40285577a76301d97c31edae28d9eb067bc
Ammar Elmihy <amaryasser2358@gmail.com>
2025-08-06 16:12:00 +0300

    add server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+app.db = router.db; // ربط الداتا بالـ app
+
+// Middlewares
+app.use(cors());
+app.use(jsonServer.defaults());
+app.use(auth);
+app.use(router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ JSON Server with Auth running on port ${PORT}`);
+});
